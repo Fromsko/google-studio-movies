56e61ae22bbaa7d5a6ed88857fdeda3864f36c80
sko From (Fromsko) <hnkong666@gmail.com>
2025-04-15 16:54:54 +0000

    还是很丑，需要加上 导航栏

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   };
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div>
       <Toaster />
       <h1 className="text-3xl font-semibold mb-4">Welcome to the Movie Streamer</h1>
 
